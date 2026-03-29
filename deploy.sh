395c694f1bfbc21ead1d3b314abb0aa7b39d08c2
Tamilarasan-23 <tamilarasan4483@gmail.com>
2026-03-29 11:52:13 +0530

    Switch logic

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,17 @@ docker build --build-arg BUILD_TIME="$(date)" -t dev-dashboard:v2 .
 echo "Running GREEN container..."
 docker run -d -p 8082:80 --name green dev-dashboard:v2
 
-echo "Switching traffic to GREEN..."
+echo "Switching traffic..."
 
-sed -i 's/proxy_pass http:\/\/blue;/proxy_pass http:\/\/green;/' nginx.conf
+if grep -q "proxy_pass http://blue;" nginx.conf; then
+    sed -i 's/proxy_pass http:\/\/blue;/proxy_pass http:\/\/green;/' nginx.conf
+    echo "Switched to GREEN"
+else
+    sed -i 's/proxy_pass http:\/\/green;/proxy_pass http:\/\/blue;/' nginx.conf
+    echo "Switched to BLUE"
+fi
 
-# Restart nginx container
 NGINX_ID=$(docker ps -q --filter ancestor=nginx)
 docker restart $NGINX_ID
 
-echo "Deployment completed successfully!"
+echo "Deployment completed!"
